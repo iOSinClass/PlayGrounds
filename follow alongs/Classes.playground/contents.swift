0caaf78814070333e506c492886bb7c7f59c4e04
Joshua Laesch <joshua.laesch@gmail.com>
2018-03-13 11:36:30 -0400

    enums and classes

--- follow alongs/Classes.playground/contents.swift
+++ follow alongs/Classes.playground/contents.swift
@@ -1,31 +1,61 @@
 // Simple class
+class BankAccount: CustomStringConvertible {
+    var name: String
+    var balance: Double
+    
+    init(name: String, balance: Double) {
+        self.name = name
+        self.balance = balance
+    }
+    
+    func withdraw(_ amount: Double) {
+        balance -= amount
+    }
+    
+    func deposit(_ amount: Double) {
+        balance += amount
+    }
+    
+    var description: String {
+        return "\(name): $\(balance)"
+    }
+}
 
 
-
-
-
-
-
-
-//var daveAccount = BankAccount(name: "Dave", balance: 100.00)
-//daveAccount.deposit(50)
-//daveAccount.withdraw(10)
-
-
-
-
+var daveAccount = BankAccount(name: "Dave", balance: 100.00)
+daveAccount.deposit(50)
+daveAccount.withdraw(10)
 
 
 // Subclass
 
+class AtmBankAccount: BankAccount {
+    var withdrawFee: Double
+    let defaultWithdrawFee: Double = 3.0 // const
+    
+    override init(name: String, balance: Double) {
+        withdrawFee = defaultWithdrawFee;
+        super.init(name: name, balance: balance)
+    }
+    
+    init(name: String, balance: Double, withDraw: Double) {
+        withdrawFee = withDraw
+        super.init(name: name, balance: balance)
+    }
+    
+    convenience init() {
+        self.init(name: "Anonymous", balance: 0)
+    }
+    
+    override func withdraw(_ amount: Double) {
+        super.withdraw(amount)
+        balance -= withdrawFee
+    }
+}
 
-
-
-
-
-//var bobAccount = AtmBankAccount()
-//bobAccount.deposit(100)
-//bobAccount.withdraw(40)
+var bobAccount = AtmBankAccount()
+bobAccount.deposit(100)
+bobAccount.withdraw(40)
 
 
 
--- follow alongs/Enums.playground/contents.swift
+++ follow alongs/Enums.playground/contents.swift
@@ -1,27 +1,58 @@
 // Basic enum
+enum Weekday {
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
 
+var today: Weekday
+today = Weekday.monday
+//today.rawValue
+var tomorrow: Weekday = .thursday
+tomorrow = .tuesday
 
-
-
-
-
-
-
+switch today {
+    
+case .monday:
+    print("margarita")
+case .tuesday:
+    print("turn up")
+case .wednesday:
+    print("weekend")
+case .thursday:
+    print("thirsty")
+case .friday:
+    print("rest")
+}
 
 // Rawvalues
 
 
-
-
-
-
-
-
-
-
-
-
-
 // Associated values and functions
+
+enum HomeworkStatus {
+    case noHomework
+    case inProgress(Int, Int)
+    case done
+    
+    func simpleDescription() -> String {
+        switch self {
+            
+        case .noHomework:
+            return "Yayyy"
+        case .inProgress(let numComplete, let totalToDo):
+            return "finished \(numComplete) out of \(totalToDo)"
+        case .done:
+            return "finished!"
+        }
+    }
+}
+
+var myHwStatus = HomeworkStatus.inProgress(2, 5)
+myHwStatus.simpleDescription()
+
+
 
 
